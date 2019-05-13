15ff4be52db882c2b0a55eb4f7f8e11063f6b2d2
della <oskardelavaux@gmail.com>
2019-05-13 17:43:16 +0200

    struct

--- Kod/SongInfo.java
+++ Kod/SongInfo.java
@@ -1,6 +1,26 @@
-
+/**
+ * this class gets the song info
+ * the information extracted is Name, Band, File path, Length
+ * 
+ * 
+ * @author oskar
+ *
+ */
 public class SongInfo {
-    public static void main(Sting[]args){
-        
-    }
+	
+	private String name;
+	private String bandName;
+	private Path path;
+	private int length;
+	
+	public SongInfo(File file){
+		this.name = 
+		this.bandName =
+		this.path = 
+		this.length = 	
+	}
+	
+	
+	
+
 }
