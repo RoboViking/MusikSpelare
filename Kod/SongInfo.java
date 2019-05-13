93689912d7fe1b1094d6ff10faaf09aca8dc5aa1
della <oskardelavaux@gmail.com>
2019-05-13 18:02:11 +0200

    song length
    added descriptions

--- Kod/SongInfo.java
+++ Kod/SongInfo.java
@@ -11,15 +11,26 @@ public class SongInfo {
 	private String name;
 	private String bandName;
 	private Path path;
-	private int length;
+	private double length;
 	
 	public SongInfo(File file){
 		this.name = 
 		this.bandName =
-		this.path = 
-		this.length = 	
+		this.path = file.getPath();
+		this.length = songLength(file);
 	}
 	
+	/**
+	 * returns the length of the song 
+	 */
+	private double songLength(File file) {
+		AudioInputStream inAudioStream = AudioSystem.getAudioInputStream(file); // gets an input stream of the file read
+		AudioFormat format = inAudioStream.getFormat(); // gets th files format 
+		long frames = inAudioStream.getFrameLength();	// gets the length of the file in frames rather than bytes
+		double time = (frames+0.0) / format.getFrameRate(); // gets the formats framerate (the number of frames played per second)
+															// and calculates the time of the song frames / framerate
+		return time;
+	}
 	
 	
 
