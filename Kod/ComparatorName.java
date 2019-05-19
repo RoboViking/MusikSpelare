ac7cb35b1708c6cd314ac8d321b238c230a8931c
della <oskardelavaux@gmail.com>
2019-05-19 14:36:45 +0200

    comparator name

---
+++ Kod/ComparatorName.java
@@ -0,0 +1,25 @@
+import java.util.Comparator;
+/**
+ * 
+ * @author Oskar de la Vaux
+ *
+ */
+public class ComparatorName implements Comparator<SongInfo>{
+	/**
+	 * compares the names of the countries
+	 */
+	public int compare(SongInfo arg0, SongInfo arg1) {
+		String name1 = arg0.returnName();
+		String name2 = arg1.returnName();
+		if(name1.compareTo(name2)<0) {
+			return -1;
+		}
+		else if(name1.compareTo(name2)>0) {
+			return 1;
+		}
+		else {
+			return 0;	
+		}
+	}
+
+}
